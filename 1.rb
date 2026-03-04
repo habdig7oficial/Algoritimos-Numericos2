118663ab1fe5ef7befe68a0ad8e1c27b2b24acb3
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2026-03-04 15:18:55 -0300

    ok

--- 1.rb
+++ 1.rb
@@ -1,3 +1,7 @@
+=begin
+    Exercício 1
+=end
+
 class Numeric
     @@arr = []
     def fact(num)
@@ -32,3 +36,38 @@ class Numeric
 end
 
 
+=begin
+    Exercício 3
+=end
+
+gabarit = Math.exp 3
+
+p5 = 3.exp 5
+err5 = gabarit - p5 
+err_rel5 = err5 / gabarit
+
+puts "Precision 5: #{p5}"
+puts "Absolute Error #{err5}"
+puts "Relative Error #{err_rel5}"
+
+
+p10 = 3.exp 10
+err10 = gabarit - p10
+err_rel10 = err10 / gabarit
+
+puts "\n\nPrecision 10: #{3.exp 10}"
+puts "Absolute Error #{err10}"
+puts "Relative Error #{err_rel10}"
+
+
+=begin
+    RESULTS:
+
+    Precision 5: 16.375
+    Absolute Error 3.710536923187668
+    Relative Error 0.18473675547622795
+
+
+    Precision 10: 20.063392857142855
+    Absolute Error 0.022144066044813115
+=end
